265e737bb44d231e68bd8ca53ac95548e7a0e80a
Owin Ekanayake <owinlakshitha@gmail.com>
2025-01-05 17:14:07 +0530

    Add Firebase to the project - Install `firebase` dependency to enable Firebase services integration.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AdminPage from "./pages/admin-page/admin-page";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login-page/login";
 import CategoriesPage from "./pages/client-page/categories";
+import TestComponent from "./pages/client-page/test";
 function App() {
   return (
     <div>
@@ -11,8 +12,10 @@ function App() {
         <Routes>
           <Route path="/admin/*" element={<AdminPage />}></Route>
           <Route path="/login" element={<LoginPage/>}></Route>
-          <Route path="categories"element={<CategoriesPage/>}></Route>
+          <Route path="/categories"element={<CategoriesPage/>}></Route>
+          <Route path="/test" element={<TestComponent></TestComponent>}></Route>
           <Route path="/*" element={<HomePage />}></Route>
+          
         </Routes>
       </BrowserRouter>
     </div>
