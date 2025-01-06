70a07af5cc351fd617ccd086cd70d0dd62769292
Owin Ekanayake <owinlakshitha@gmail.com>
2025-01-06 21:18:38 +0530

    Add media upload functionality with Firebase storage integration

--- src/utils/mediaUpload.js
+++ src/utils/mediaUpload.js
@@ -1,6 +1,19 @@
 import app from "../config/firebase"
-import { getStorage } from "firebase/storage";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 
-const storage = getStorage(app, "gs://my-custom-bucket");
+const storage = getStorage(app, "gs://hotel-management-1a316.firebasestorage.app");
 
-export default storage;
+export default function uploadMedia(file){
+    if(file== null) {
+        return;
+    }
+
+    const fileRef = ref(storage, file.name);
+    uploadBytes(fileRef,file).then((snapshot)=>{
+        getDownloadURL(snapshot.ref).then((url)=>{
+            console.log(url);
+        })
+        
+    })
+}
+
